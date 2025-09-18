6aa2e641a2a8ac5757fe71815106391e494ddaa7
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-09-18 19:02:00 -0300

    chore: changing navbar name

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -61,7 +61,7 @@ const Navigation = () => {
             className="flex-shrink-0"
           >
             <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-              Rafael Menegheti
+              Rafael
             </h1>
           </motion.div>
 
